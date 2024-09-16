acf86bbf9511f7f4823bd08cdf5eef65ab716dcf
Marcus Whybrow <marcus@whybrow.uk>
2024-09-16 14:52:24 +0100

    Refactor flake for simplicity, add plugin comments

--- flake.nix
+++ flake.nix
@@ -3,56 +3,62 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    harpoon.url = "github:NixOS/nixpkgs/8cfef6986adfb599ba379ae53c9f5631ecd2fd9c";
   };
 
   outputs = inputs: let 
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 
-    config = pkgs.neovimUtils.makeNeovimConfig {
+    # For all args available for makeNeovimConfig see 
+    # https://github.com/NixOS/nixpkgs/blob/9b79a05ae1311685f77dafbdfc88e81e390b22f1/pkgs/applications/editors/neovim/utils.nix#L26
+    neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (pkgs.neovimUtils.makeNeovimConfig {
       withPython3 = false; 
-      extraPython3Packages = _: [ ];
-      withNodeJs = false;
       withRuby = false;
-      extraLuaPackages = _: [];
-      customRC = "";
-      extraName = "";
-      withPython2 = false;
-      vimAlias = false; 
-      viAlias = false;
-      wrapRc = false;
-      neovimRcContent = "";
+
       plugins = [
-        pkgs.vimPlugins.telescope-nvim
-        pkgs.vimPlugins.telescope-fzf-native-nvim
-        pkgs.vimPlugins.nvim-web-devicons
-        pkgs.vimPlugins.undotree
-        pkgs.vimPlugins.trouble-nvim
-        pkgs.vimPlugins.vim-nix
-        pkgs.vimPlugins.luasnip
-        pkgs.vimPlugins.nvim-lspconfig
-        pkgs.vimPlugins.nvim-cmp
-        pkgs.vimPlugins.cmp_luasnip
-        pkgs.vimPlugins.cmp-nvim-lsp
-        pkgs.vimPlugins.cmp-nvim-lua
-        pkgs.vimPlugins.cmp-buffer
-        pkgs.vimPlugins.cmp-path
-        pkgs.vimPlugins.cmp-cmdline
-        pkgs.vimPlugins.cmp-cmdline-history
-        pkgs.vimPlugins.cmp-git
-        pkgs.vimPlugins.vim-commentary
-        pkgs.vimPlugins.vim-surround
-        pkgs.vimPlugins.vim-fugitive
-        pkgs.vimPlugins.gitsigns-nvim
+        # USER INTERFACE
+
+        # Custom status line configuration (very important)
         pkgs.vimPlugins.lualine-nvim
+
+        # Color theme
         pkgs.vimPlugins.catppuccin-nvim
-        pkgs.vimPlugins.plenary-nvim
-        pkgs.vimPlugins.vim-startuptime
+
+        # Makes the background of CSS colors the actual color defined
         pkgs.vimPlugins.nvim-highlight-colors 
 
-        inputs.harpoon.legacyPackages.x86_64-linux.vimPlugins.harpoon
+        # BG, sync your terminal background with your Neovim background color
+        (pkgs.vimUtils.buildVimPlugin rec {
+          pname = "bg-nvim";
+          version = "1c95261cc5e3062e3b277fc5c15d180d51a40f62";
+          src = pkgs.fetchFromGitHub {
+            owner = "typicode";
+            repo = "bg.nvim";
+            rev = version; 
+            sha256 = "sha256-ZocdEdw7m6gVQap0MFr1uymIkHnX9ewjWmR7fYVR9Ko=";
+          };
+        })
 
-        # nvim-treesitter
+        # NAVIGATION
+
+        # Popup project search box with file preview
+        pkgs.vimPlugins.telescope-nvim
+
+        # Fuzzy search for telescope
+        pkgs.vimPlugins.telescope-fzf-native-nvim
+
+        # Telescope file icons
+        pkgs.vimPlugins.nvim-web-devicons
+
+        # Sidebar that can save your ass when you've overriden undos
+        pkgs.vimPlugins.undotree
+
+        # Pin specific files to specific keys for instanst switching
+        pkgs.vimPlugins.harpoon
+
+        # SYNTAX HIGHLIGHTING
+
+        # Treesitter parses files into trees of tokens for cheap syntax 
+        # highlighting and easy code folding. 
         (pkgs.vimPlugins.nvim-treesitter.withPlugins (treesitter: [
           treesitter.nix 
           treesitter.go 
@@ -71,19 +77,71 @@
           treesitter.javascript
         ]))
 
-        # templ-vim
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "templ-vim";
-          version = "5cc48b93a4538adca0003c4bc27af844bb16ba24";
-          src = pkgs.fetchFromGitHub {
-            owner = "joerdav";
-            repo = "templ.vim";
-            rev = "5cc48b93a4538adca0003c4bc27af844bb16ba24";
-            sha256 = "sha256-YdV8ioQJ10/HEtKQy1lHB4Tg9GNKkB0ME8CV/+hlgYs=";
-          };
-        })
+        # LANGUAGE SERVER PROTOCOL
 
-        # vim-eel2
+        # LSP Zero knows about every language server in existance, and uses 
+        # sensible defaults, whilst allowing overrides when needed.
+        pkgs.vimPlugins.lsp-zero-nvim
+
+        # Lists project-wide LSP errors for you to fix
+        pkgs.vimPlugins.trouble-nvim
+
+        # COMPLETION
+
+        # Code completion popup
+        pkgs.vimPlugins.nvim-cmp
+
+        # Code completion of common code patterns
+        pkgs.vimPlugins.luasnip
+        pkgs.vimPlugins.cmp_luasnip
+
+        # Language Server Protocol configurations & completion
+        pkgs.vimPlugins.nvim-lspconfig
+        pkgs.vimPlugins.cmp-nvim-lsp
+
+        # Completion for Neovim Lua API
+        pkgs.vimPlugins.cmp-nvim-lua
+
+        # Completion from word extant in the current buffer (file being edited)
+        pkgs.vimPlugins.cmp-buffer
+
+        # Completion from working directory paths
+        pkgs.vimPlugins.cmp-path
+
+        # Completion inside the Neovim command line too
+        pkgs.vimPlugins.cmp-cmdline
+
+        # Complete Neovim command line from it's own history
+        pkgs.vimPlugins.cmp-cmdline-history
+
+        # Complete from Git refs and such (I think)
+        pkgs.vimPlugins.cmp-git
+
+        # EDITING
+
+        # Easily comment out blocks of code
+        pkgs.vimPlugins.vim-commentary
+
+        # Parenthesis, string quotes, HTML tag wrapping, all that stuff
+        pkgs.vimPlugins.vim-surround
+
+        # SOURCE CONTROL
+
+        # Git commits (and more) inside of Neovim
+        pkgs.vimPlugins.vim-fugitive
+
+        # Color git additions and deletions for each buffer
+        pkgs.vimPlugins.gitsigns-nvim
+
+        # LANGUAGE SPECIFIC
+
+        # Syntax highlighting for Nix (However the Nix LSP may supersede this)
+        pkgs.vimPlugins.vim-nix
+
+        # Golang's Templ templating language
+        pkgs.vimPlugins.templ-vim
+
+        # REAPER's (DAW) JSFX plugin language
         (pkgs.vimUtils.buildVimPlugin {
           pname = "vim-eel2";
           version = "2019-11-16";
@@ -95,98 +153,76 @@
           };
         })
 
-        # lsp-zero-nvim
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "lsp-zero-nvim";
-          version = "2.x";
-          src = pkgs.fetchFromGitHub {
-            owner = "VonHeikemen";
-            repo = "lsp-zero.nvim";
-            rev = "eb278c30b6c50e99fdfde52f7da0e0ff8d17c07e";
-            sha256 = "sha256-C2LvhoNdNXRyG+COqVZv/BcUh6y82tajXipsqdySJJQ=";
-          };
-        })
+        # UTILS
 
-        # bg-nvim
-        (pkgs.vimUtils.buildVimPlugin rec {
-          pname = "bg-nvim";
-          version = "1c95261cc5e3062e3b277fc5c15d180d51a40f62";
-          src = pkgs.fetchFromGitHub {
-            owner = "typicode";
-            repo = "bg.nvim";
-            rev = version; 
-            sha256 = "sha256-ZocdEdw7m6gVQap0MFr1uymIkHnX9ewjWmR7fYVR9Ko=";
-          };
-        })
+        # A utils library for other plugins (not sure which)
+        pkgs.vimPlugins.plenary-nvim
+
+        # Measure's Neovim's startup time for debugging any issues there
+        pkgs.vimPlugins.vim-startuptime
       ];
-    };
+    });
 
-    neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped config;
-
-    inPath = pkgs.lib.makeBinPath (with pkgs; [
-      nil # nix
-      gopls # golang
-      nodePackages."@tailwindcss/language-server" # tailwind CSS processor
-      nodePackages.bash-language-server # BASH
-      nodePackages.vscode-langservers-extracted # html css json eslint
-      nodePackages.yaml-language-server # yaml
-      rust-analyzer #rust
-      marksman # markdown
-      typescript-language-server # typescript jsdoc
-    ]);
-
-    initFile = pkgs.writeText "neovim-init.lua" (builtins.readFile ./init.lua);
-
-    init = pkgs.runCommand "neovim-init.lua" {} ''
-      mkdir --parents $out/share/nvim
-      ln --symbolic ${initFile} $out/share/nvim/init.lua
-    '';
-
-    wrapper = pkgs.runCommand "neovim-wrapper" {
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-    } ''
-      makeWrapper ${neovim}/bin/nvim $out/bin/nvim \
-        --add-flags "-u ${init}/share/nvim/init.lua" \
-        --suffix PATH : ${inPath}
-      ln --symbolic $out/bin/nvim  $out/bin/vim
-      '';
-
-    wrapper-no-lsp = pkgs.runCommand "neovim-wrapper-no-lsp" {
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-    } ''
-      makeWrapper ${neovim}/bin/nvim $out/bin/nvim \
-        --add-flags "-u ${init}/share/nvim/init.lua" \
-        --suffix PATH : ${inPath}
-      ln --symbolic $out/bin/nvim $out/bin/vim
-    '';
-
-    fishAbbrs = pkgs.writeTextDir "share/fish/vendor_conf.d/marcuswhybrow-neovim.fish" ''
-      if status is-interactive
-        abbr --add t vim ~/obsidian/Timeline/$(date +%Y-%m-%d).md
-        abbr --add y vim ~/obsidian/Timeline/$(date +%Y-%m-%d --date yesterday).md
-      end
-    '';
   in {
     packages.x86_64-linux.nvim = pkgs.symlinkJoin {
       name = "nvim";
-      paths = [ 
-        wrapper 
-        init 
-        pkgs.neovim 
-        fishAbbrs 
-      ];
       meta.description = "Neovim including LSP config and servers";
-    };
-
-    packages.x86_64-linux.nvim-no-lsp = pkgs.symlinkJoin {
-      name = "nvim";
       paths = [ 
-        wrapper-no-lsp 
-        init 
+        (pkgs.runCommand "neovim-wrapper" {
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+        } ''
+          mkdir --parents $out/share/nvim
+          ln --symbolic \
+          ${pkgs.writeText "neovim-init.lua" (builtins.readFile ./init.lua)} \
+          $out/share/nvim/init.lua
+
+          makeWrapper ${neovim}/bin/nvim $out/bin/nvim \
+            --add-flags "-u $out/share/nvim/init.lua" \
+            --suffix PATH : ${pkgs.lib.makeBinPath [
+              # Nix LSP server
+              pkgs.nil
+
+              # Golang LSP server
+              pkgs.gopls
+
+              # Tailwind LSP server
+              pkgs.nodePackages."@tailwindcss/language-server"
+
+              # BASH LSP server
+              pkgs.nodePackages.bash-language-server
+
+              # HTML, CSS, JSON & Eslint LSP servers
+              pkgs.nodePackages.vscode-langservers-extracted
+
+              # YAML LSP server
+              pkgs.nodePackages.yaml-language-server
+
+              # Rust LSP server
+              pkgs.rust-analyzer
+
+              # Markdown LSP server
+              pkgs.marksman
+
+              # Typescript/JSDoc LSP server
+              pkgs.typescript-language-server
+
+              # Required by pkgs.vimPlugins.cmp-git
+              pkgs.curl
+
+              # Required by the file content's search of pkgs.vimPlugins.telescope-nvim
+              pkgs.ripgrep
+
+              # Required by the fuzzy finder search for pkgs.vimPlugins.telescope-nvim
+              pkgs.fzf
+            ]}
+
+          # Alias vim to nvim
+          ln --symbolic $out/bin/nvim  $out/bin/vim
+        '')
+
+        # The original Neovim package
         pkgs.neovim 
-        fishAbbrs
       ];
-      meta.description = "Neovim with LSP configured but without LSP *servers*";
     };
 
     packages.x86_64-linux.default = inputs.self.packages.x86_64-linux.nvim;
