575c8c13b152b532ab817a424ceb8abf2e71edb7
Marcus Whybrow <marcus@whybrow.uk>
2024-11-26 23:02:17 +0000

    Fix for neovim/neovim#30985 pending v0.10.3

--- init.lua
+++ init.lua
@@ -537,3 +537,17 @@ vim.keymap.set("n", "<leader>gp", ":Git push<cr>", { desc = '[G]it [P]ush' })
 vim.opt.termguicolors = true
 require("nvim-highlight-colors").setup()
 
+-- [[rust-analyzer]]
+
+-- Fix for https://github.com/neovim/neovim/issues/30985 whereby rust-analyzer
+-- constantly lags user input with the message "server cancelled the request".
+-- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
